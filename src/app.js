75826963b79710cc37abd1b3923c12bf67b9bc8f
vendy <vendy9229@163.com>
2016-04-21 18:25:07 +0800

    增加 vue-router使用demo,后续继续完善

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@
 // var Router = require("vue-router");
 require('./style/animate.css')
 require('./style/style.css')
-require("./javascripts/test")
+// require("./javascripts/test")
+
 
 import Vue from 'vue'
 import Router from 'vue-router'
+// import { domain, fromNow } from './filters'
+var App =  require('./components/App.vue')
+import ListView from './components/ListView.vue'
 
-// console.log("1111")
+// install router
+Vue.use(Router)
+
+// routing
+var router = new Router()
+
+router.map({
+	'/ListView':{
+		component:ListView
+	}
+})
+
+router.start(App, '#app')
