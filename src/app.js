056944132ee49b05c6eef352d616159fddef8880
vendy <vendy9229@163.com>
2016-04-22 18:38:51 +0800

    增加部分vue事件进行试验

--- src/app.js
+++ src/app.js
@@ -2,25 +2,13 @@
 // var Router = require("vue-router");
 require('./style/animate.css')
 require('./style/style.css')
-	// require("./javascripts/test")
+// require("./javascripts/test")
 
 var Vue = require("vue")
 import Router from 'vue-router'
 // import { domain, fromNow } from './filters'
 import App from './components/App.vue'
 import ListView from './components/ListView.vue'
-// var test = require('./views/ListView')
-import testview from './views/ListView'
-
-var template = require('./components/test.html');
-var MyComponent = Vue.extend({
-  template: template,
-  data(){
-  	return {
-  		viewName:"list in appj55"
-  	}
-  }
-})
 
 // install router
 Vue.use(Router)
@@ -33,7 +21,7 @@ router.map({
 		component: ListView
 	},
 	'/test': {
-		component: testview
+		component: ListView
 	}
 })
 
