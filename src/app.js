4774d9ab83d8b8b5fc073a8d23c255f5dc4b3496
vendy <vendy9229@163.com>
2016-05-06 19:12:55 +0800

    组件数据继承

--- src/app.js
+++ src/app.js
@@ -4,28 +4,35 @@ require('./lib/lib-flexible/flexible.css')
 require('./lib/lib-flexible/flexible.js')
 
 
+
 var Vue = require("vue")
 import Router from 'vue-router'
 // import { domain, fromNow } from './filters'
 import App from './components/App.vue'
 import ListView from './components/ListView.vue'
+
+import HomePage from './components/HomePage.vue'
 Vue.transition('trans', {
 		enterClass: 'slideInRight',
 		leaveClass: 'slideOutRight'
 	})
 	// install router
 Vue.use(Router)
-
-// routing
 var router = new Router({
 	transitionOnLoad: false
 })
 
+router.afterEach(function(transition) {
+		// com.jugeStyle();
+		console.log('成功浏览到: ' + transition.to.path)
+	})
+	// routing
+
 router.map({
-	'/ListView': {
-		component: ListView
+	'/': {
+		component: HomePage
 	},
-	'/test': {
+	'/ListView': {
 		component: ListView
 	}
 })
